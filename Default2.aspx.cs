bd596eeb10d59fee213d72f679147b3740a1c313
regu_u11 <reinguet@web.de>
2015-07-01 09:40:43 +0200

    Deafault2

---
+++ Default2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    int zahl = 20;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+         TextBoxZahl.Text = zahl.ToString();
+    }
+    protected void ButtonIncrease_Click(object sender, EventArgs e)
+    {
+        Int32 z = Convert.ToInt32(TextBoxZahl.Text);
+        z++;
+        TextBoxZahl.Text = z.ToString();
+        if (z % 2 == 1)
+        {
+            TextBoxZahl.BackColor = System.Drawing.Color.Yellow;
+        }
+        else TextBoxZahl.BackColor = System.Drawing.Color.Green; 
+    }
+}
